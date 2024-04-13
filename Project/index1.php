525e38ce0a824cbc46e497ad092bd184a1807b04
DASSLER <antonov_os@edu.surgu.ru>
2024-04-13 11:13:30 +0500

    Add file

---
+++ Project/index1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<main>
+    <form action="register.php" method = 'post'>
+        <input type="text" placeholder="login" name = 'login'>
+        <input type="text" placeholder="password" name = 'pass'>
+        <input type="text" placeholder="repeat_password" name = 'repeats'>
+        <input type="text" placeholder="email" name = 'email'>
+        <button type = 'submit'> Зарегистрироваться </button>
+    </form>
+
+
+    <form action="login.php" method = 'post'>
+        <input type="text" placeholder="login" name = 'login'>
+        <input type="text" placeholder="password" name = 'pass'>
+        <button type = 'submit'>Войти</button>
+    </form>
+</main>
+</body>
+</html>
